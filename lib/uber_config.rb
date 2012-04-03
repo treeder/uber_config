d9d7db02d4a1c7d4b9bdc1998ced6411411c4467
Travis Reeder <treeder@gmail.com>
2012-04-03 09:52:37 -0700

    Removed a bunch of puts.

--- lib/uber_config.rb
+++ lib/uber_config.rb
@@ -7,6 +7,7 @@ module UberConfig
 
     # First check for abt config
     if defined? $abt_config
+      puts "$abt_config found."
       @config = $abt_config
       return @config
     end
@@ -23,41 +24,36 @@ module UberConfig
       file = options[:file]
     end
 
-    p Kernel.caller
+    #p Kernel.caller
     caller_file = caller[0][0...(caller[0].index(":in"))]
     caller_file = caller_file[0...(caller_file.rindex(":"))]
-    p caller_file
+    #p caller_file
     caller_dir = File.dirname(caller_file)
-    p caller_dir
+    #p caller_dir
     caller_dir_split = caller_dir.split("/")
-    p caller_dir_split
+    #p caller_dir_split
     auto_dir_name = caller_dir_split.last
     if auto_dir_name == "test"
       caller_dir_split.pop
       auto_dir_name = caller_dir_split.last
     end
-    p auto_dir_name
+    #p auto_dir_name
 
     # Now check near caller file
     dir_and_file = dir.nil? ? [] : dir.dup
     dir_and_file << file
-    p dir_and_file
+    #p dir_and_file
     location = File.join(dir_and_file)
-    p location
+    #p location
     cf = File.expand_path(location, caller_dir)
-    puts "cf=" + cf.inspect
-    if File.exist?(cf)
-      @config = YAML::load_file(cf)
-      return @config
-    end
+    @config = load_from(cf)
+    return @config if @config
 
     # and working directory
     cf = File.expand_path(location)
-    puts "cf=" + cf.inspect
-    if File.exist?(cf)
-      @config = YAML::load_file(cf)
-      return @config
-    end
+    @config = load_from(cf)
+    return @config if @config
+
 
     # Now check in Dropbox
     dropbox_folders = ["~", "Dropbox", "configs"]
@@ -68,16 +64,21 @@ module UberConfig
     end
     dropbox_folders << file
     cf = File.expand_path(File.join(dropbox_folders))
-    puts "cf=" + cf.inspect
-    if File.exist?(cf)
-      @config = YAML::load_file(cf)
-      return @config
-    end
-
-
+    @config = load_from(cf)
+    return @config if @config
 
     # couldn't find it
+    puts "Could not find config file."
     return nil
 
+  end
+
+  def load_from(cf)
+    puts "Checking for config file at #{cf}..."
+    if File.exist?(cf)
+      config = YAML::load_file(cf)
+      return config
+    end
+    nil
   end
 end
